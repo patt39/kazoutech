91407b08abcb5448c70f8b115fbc0ef128096950
Boclair Temgoua <boclair.temgoua@double-you.it>
2019-09-05 12:59:48 +0200

    role fix correction

--- resources/views/admin/role/edit.blade.php
+++ resources/views/admin/role/edit.blade.php
@@ -5,73 +5,75 @@
 @endsection
 
 @section('content')
-    <div class="content">
+    <div class="main-panel">
+        <top-nav></top-nav>
+        <div class="content">
 
-        <div class="container-fluid">
-            <!-- le can('est la protection de la permission ') -->
-        @can('edit-role')
+            <div class="container-fluid">
+                <!-- le can('est la protection de la permission ') -->
+            @can('edit-role')
 
-            <!-- form init -->
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-md-12 expo">
-                                    {!! Form::model($role, ['files'=> 'true','method' => 'PATCH','route' => ['roles.update', $role->id], 'id' => 'RegisterValidation']) !!}
-                                    <div class="card ">
-                                        <div class="card-header card-header-warning card-header-icon">
-                                            <div class="card-icon">
-                                                <i class="material-icons">how_to_reg</i>
+                <!-- form init -->
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-12 expo">
+                                        {!! Form::model($role, ['files'=> 'true','method' => 'PATCH','route' => ['roles.update', $role->id], 'id' => 'RegisterValidation']) !!}
+                                        <div class="card ">
+                                            <div class="card-header card-header-warning card-header-icon">
+                                                <div class="card-icon">
+                                                    <i class="material-icons">how_to_reg</i>
+                                                </div>
+                                                <h4 class="card-title">
+                                                    <b>Update Role</b>
+                                                </h4>
                                             </div>
-                                            <h4 class="card-title">
-                                                <b>Update Role</b>
-                                            </h4>
-                                        </div>
 
-                                        @include('admin.role.form')
+                                            @include('admin.role.form')
 
-                                        <br><br>
-                                        <hr>
-                                        <div class="submit">
-                                            <div class="text-center">
-                                                <a href="{{route('roles.index')}}" class="btn btn-info btn-raised btn-round">
+                                            <br><br>
+                                            <hr>
+                                            <div class="submit">
+                                                <div class="text-center">
+                                                    <a href="{{route('roles.index')}}" class="btn btn-info btn-raised btn-round">
                                     <span class="btn-label">
                                         <i class="material-icons">undo</i>
                                     </span>
-                                                    <b>Back</b>
-                                                </a>
-                                                <button type="submit" class="btn btn-success btn-raised btn-round">
+                                                        <b>Back</b>
+                                                    </a>
+                                                    <button type="submit" class="btn btn-success btn-raised btn-round">
                                     <span class="btn-label">
                                         <i class="material-icons">save_alt</i>
                                     </span>
-                                                    <b>Update</b>
-                                                </button>
+                                                        <b>Update</b>
+                                                    </button>
+                                                </div>
+                                                <br>
                                             </div>
                                             <br>
                                         </div>
-                                        <br>
+                                        {!! Form::close() !!}
                                     </div>
-                                    {!! Form::close() !!}
                                 </div>
+
                             </div>
 
+
                         </div>
-
-
                     </div>
-                </div>
-            @endcan
-        </div>
+                @endcan
+            </div>
+        <footer-admin></footer-admin>
+    </div>
+@endsection
+@section('script')
 
+<script>
+    $(function () {
+        //Initialize Select2 Elements
+        $('.select2').select2()
 
-        @endsection
-        @section('script')
-
-            <script>
-                $(function () {
-                    //Initialize Select2 Elements
-                    $('.select2').select2()
-
-                })
-            </script>
+    })
+</script>
 @endsection
--- resources/views/layouts/admin/_script.blade.php
+++ resources/views/layouts/admin/_script.blade.php
@@ -49,6 +49,25 @@
     function autoRefresher(){self.location.reload(true);}
 </script>
 -->
+<script>
+    $(document).ready(function(){
+        $('body').append(
+            '<a id="toTop" class="btn btn-outline-success btn-sm" href="#" title="Go to Top Page">' +
+            '<i class="material-icons top-button">arrow_upward</i>' +
+            '</a>');
+        $(window).scroll(function () {
+            if ($(this).scrollTop() != 0) {
+                $('#toTop, #search-top').fadeIn();
+            } else {
+                $('#toTop, #search-top').fadeOut();
+            }
+        });
+        $('#toTop, #search-top').click(function(){
+            $("html, body").animate({ scrollTop: 0 }, 600);
+            return false;
+        });
+    });
+</script>
 
 
 @if(config('app.env') != 'local')
