9229a2cb47a9ad98b9cdd614e33d9d6e5c8f9516
Boclair Temgoua <temgoua2013@gamil.com>
2019-09-25 00:48:43 +0200

    add date

---
+++ app/Http/Controllers/Admin/Info/LegalnoticeController.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace App\Http\Controllers\Admin\info;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Info\LegalnoticeResource;
+use App\Model\admin\info\legalnotice;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\HttpFoundation\Response;
+
+class LegalnoticeController extends Controller
+{
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+        // Middleware lock account
+        //$this->middleware('auth.lock');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function index()
+    {
+        return view('admin.info.legal_notice.index');
+    }
+
+    public function api()
+    {
+        return LegalnoticeResource::collection(Legalnotice::with('user')->latest()->get());
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view( 'admin.info.legal_notice.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'body'=>'required',
+        ]);
+
+        $legalnotice = new Legalnotice;
+        $legalnotice->body = $request->body;
+        $legalnotice->user_id = auth()->user()->id;
+        $legalnotice->status = '0';
+
+        $legalnotice->save();
+
+        return response('Created',Response::HTTP_CREATED);
+    }
+
+    /**
+     * cette partie consite a activer et a desactiver
+     * @param $id
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function disable($id)
+    {
+        DB::table('legalnotices')
+            ->where('id',$id)
+            ->update([
+                'status' => 0,
+                'user_id' => auth()->user()->id,
+            ]);
+        return response('deactivated',Response::HTTP_ACCEPTED);
+    }
+
+    public function active($id)
+    {
+        DB::table('legalnotices')
+            ->where('id',$id)
+            ->update([
+                'status' => 1,
+                'user_id' => auth()->user()->id,
+            ]);
+
+        return response('Activated',Response::HTTP_ACCEPTED);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @return LegalnoticeResource|LegalnoticeResource|\Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $legalnotice = new LegalnoticeResource(legalnotice::where('id', $id)->findOrFail($id));
+        return $legalnotice;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $legalnotice = legalnotice::where('id', $id)->findOrFail($id);
+         return view('site.order.legalnotice.edit', compact('legalnotice'));
+
+      }
+    public function view($slug)
+    {
+        $legalnotice = new LegalnoticeResource(legalnotice::where('slug', $slug)->firstOrFail());
+        return $legalnotice;
+    }
+
+
+    public function vector(legalnotice $legalnotice)
+    {
+        return view( 'site.order.legalnotice.view', [
+            'legalnotice' => $legalnotice,
+        ]);
+    }
+
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\admin\howtoregister  $howtoregister
+     * @return array|\Illuminate\Http\Response
+     */
+    public function update(Request $request,$id)
+    {
+        $this->validate($request,[
+            'body'=>'required',
+        ]);
+        $legalnotice = legalnotice::findOrFail($id);
+
+        $legalnotice->body = $request->body;
+        $legalnotice->user_id = auth()->user()->id;
+
+        $legalnotice->save();
+
+        return ['message' => 'updated successfully'];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Model\admin\howtoregister  $howtoregister
+     * @return array|\Illuminate\Http\Response
+     */
+    public function destroy(Request $request,$id)
+    {
+        $legalnotice = Legalnotice::findOrFail($id);
+        $legalnotice->delete();
+
+        return ['message' => 'Deleted successfully '];
+    }
+}
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,112 @@
-@extends('layouts.app')
+@extends('inc.site._main')
+@section('title', '')
+
+@section('style')
+@endsection
+
+@section('init')
+    <!-- Site wrapper -->
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+    <main>
+        <section class="section section-shaped section-lg">
+            <div class="shape shape-style-1 bg-gradient-default">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div class="container pt-lg-md">
+                <div class="row justify-content-center">
+                    <div class="col-lg-5">
+                        <div class="card bg-secondary shadow border-0">
+                            <div class="card-header bg-white pb-5">
+                                <div class="text-muted text-center mb-3"><small>Sign in with</small></div>
+                                <div class="btn-wrapper text-center">
+                                    <a href="#" class="btn btn-neutral btn-icon">
+                                        <span class="btn-inner--icon">
+                                          <img alt="image" src="/assets/site/assets/img/icons/common/github.svg">
+                                        </span>
+                                        <span class="btn-inner--text">Github</span>
+                                    </a>
+                                    <a href="#" class="btn btn-neutral btn-icon">
+                                        <span class="btn-inner--icon">
+                                          <img alt="image" src="/assets/site/assets/img/icons/common/google.svg">
+                                        </span>
+                                        <span class="btn-inner--text">Google</span>
+                                    </a>
                                 </div>
                             </div>
+                            <div class="card-body px-lg-5 py-lg-5">
+                                <div class="text-center text-muted mb-4">
+                                    <small>Or sign in with credentials</small>
+                                </div>
+                                <form method="POST" action="{{ route('login') }}">
+                                    @csrf
+                                    <div class="form-group mb-3">
+                                        <div class="input-group input-group-alternative">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                                            </div>
+                                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                            @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group input-group-alternative">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                                            </div>
+                                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" name="password" required autocomplete="current-password">
+
+                                            @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="custom-control custom-control-alternative custom-checkbox">
+                                        <input class="custom-control-input" id="remember" name="remember" type="checkbox" {{ old('remember') ? 'checked' : '' }}>
+                                        <label class="custom-control-label" for="customCheckLogin">
+                                            <span>Remember me</span>
+                                        </label>
+                                    </div>
+                                    <div class="text-center">
+                                        <button type="submit" class="btn btn-primary my-4">{{ __('Sign in') }}</button>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
+                        <div class="row mt-3">
+                            <div class="col-6">
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                    <a class="text-light" href="{{ route('password.request') }}">
+                                       <small>{{ __('Forgot Your Password?') }}</small>
                                     </a>
                                 @endif
                             </div>
+                            <div class="col-6 text-right">
+                                <a href="{{ route('register') }}" class="text-light"><small>Create new account</small></a>
+                            </div>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-</div>
+        </section>
+    </main>
+@endsection
+
+@section('script')
+
 @endsection
