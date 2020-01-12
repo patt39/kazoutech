9bb1e594d41b8e5fd2c1c66a8f8d030600bce610
Boclair Temgoua <temgoua2013@gamil.com>
2020-01-12 16:53:07 +0100

    mise a jour data

--- resources/views/auth/devenir_charbonneur.blade.php
+++ resources/views/auth/devenir_charbonneur.blade.php
@@ -19,6 +19,18 @@
                     <div class="col-lg-10 mx-auto text-center">
                         <h3 class="display-3 text-white">Bienvenue chez KazouTech !</h3>
                         <h5 class="text-white">Développez votre activité de service à la personne, choisissez vos jobs et augmentez vos revenus.</h5>
+                        <h5 class="text-white">Suivez nous</h5>
+                        <div class="mt-4 mb-4 text-center social-container">
+                            <a href="#"  class="btn btn-facebook btn-md">
+                                <span class="btn-inner--icon"><i class="fab fa-facebook"></i></span>
+                            </a>
+                            <a href="#"  class="btn-instagram btn btn-md">
+                                <span class="btn-inner--icon"><i class="fab fa-instagram"></i></span>
+                            </a>
+                            <a href="#"  class="btn btn-twitter btn-md">
+                                <span class="btn-inner--icon"><i class="fab fa-twitter"></i></span>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -59,9 +71,6 @@
                                 <div class="mt-4 mb-4 text-center social-container">
                                     <button type="button" class="btn btn-facebook btn-md">
                                         <span class="btn-inner--icon"><i class="fab fa-facebook"></i></span>
-                                    </button>
-                                    <button type="button" class="btn-instagram btn btn-md">
-                                        <span class="btn-inner--icon"><i class="fab fa-instagram"></i></span>
                                     </button>
                                     <button type="button" class="btn btn-twitter btn-md">
                                         <span class="btn-inner--icon"><i class="fab fa-twitter"></i></span>
