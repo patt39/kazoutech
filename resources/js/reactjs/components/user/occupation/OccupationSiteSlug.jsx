c910377f45339b1a8156e17c346a1455ae77422d
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2020-03-11 13:44:24 +0100

    Reformulation site

--- resources/js/reactjs/components/user/occupation/OccupationSiteSlug.jsx
+++ resources/js/reactjs/components/user/occupation/OccupationSiteSlug.jsx
@@ -33,7 +33,6 @@ class OccupationSiteSlug extends Component {
             <>
                 <div className="landing-page">
                     <NavUserSIte/>
-
                     <div className="wrapper">
                         <OccupationHeader {...occupation}/>
                         <div className="cd-section" id="accordion">
@@ -42,108 +41,109 @@ class OccupationSiteSlug extends Component {
                                     <div className={'row'}>
                                         <div className="container text-center">
                                             <div className="row mb-5">
-                                                <div className="col-md-8 mx-auto">
-                                                    <h3 className="display-3">Nos offres en quelque clicks choisissez
-                                                        votre
-                                                        categorie</h3>
+                                                <div className="col-md-8 mx-auto mt-5">
+                                                    <h3 className="display-3">Choisissez votre catègorie en quelques clicks parmis nos offres</h3>
                                                 </div>
                                             </div>
-                                            <div className="row align-items-center">
-                                                {occupationCategories.map((item) => (
-                                                    <div key={item.id} className="col-lg-4">
-                                                        <div className="card card-background"
-                                                             style={{backgroundImage: "url(" + item.photo + ")"}}>
-                                                            <div className="card-body text-center">
-                                                                <Link
-                                                                    to={`/occupations/${occupation.slug}/${item.slug}/`}>
-                                                                    <h2 className="card-title">{item.name}</h2>
-                                                                </Link>
-                                                                <Link
-                                                                    to={`/occupations/${occupation.slug}/${item.slug}/`}
-                                                                    className={`btn btn-sm btn-${item.color.name}`}>
-                                                                    En savoir plus
-                                                                </Link>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                ))}
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                    <div className="section features-7">
-                                        <div className="container">
-                                            <div className="row justify-content-center mt-5">
-                                                <div className="col-lg-12">
-
-                                                    <div className="container">
-                                                        <div className="row">
-                                                            <div className="col-md-8 mr-auto ml-auto text-center">
-                                                                <h4 className="title mb-5">Nous sommes plus proche de
-                                                                    vous !</h4>
-                                                            </div>
-                                                        </div>
-                                                        <CitySite/>
-
-                                                        <br/>
-                                                        {allblogbyoccupations.length > 0 && (
-
-                                                            <div className="row">
-                                                                <div className="col-md-12 mx-auto">
-                                                                    <h4 className="title">Tous nos articles
-                                                                        sur <strong>{occupation.name} </strong> sont sur
-                                                                        notre
-                                                                        blog</h4>
-                                                                    <br/>
-                                                                    <div className="row">
-
-                                                                        {allblogbyoccupations.map((item) => (
-                                                                            <div className="col-lg-4 col-md-6">
-                                                                                <div className="card card-blog card-plain">
-                                                                                    <div className="card-image shadow">
-                                                                                        <Link to={`/blog/${occupation.slug}/${item.slug}/`}>
-                                                                                            <img className="img rounded"
-                                                                                                 src={item.photo}/>
-                                                                                        </Link>
-                                                                                    </div>
-                                                                                    <div className="card-body">
-
-                                                                                        <div className="text-center">
-                                                                                            <Link to={`/blog/${occupation.slug}/`} className={`btn btn-sm btn-${item.color.name}`}>
-                                                                                                {occupation.name}
-                                                                                            </Link>
-                                                                                        </div>
-
-                                                                                        <h6 className="card-title">
-                                                                                            <Link to={`/blog/${occupation.slug}/`} >{item.title}</Link>
-                                                                                        </h6>
-                                                                                        <p className="card-description">
-                                                                                            <b dangerouslySetInnerHTML={{__html: (item.body.length > 164 ? item.body.substring(0, 164) + "..." : item.body)}}/>
-                                                                                            <Link to={`/blog/${occupation.slug}/${item.slug}/`} >lire la suite</Link>
-                                                                                        </p>
-                                                                                    </div>
-                                                                                </div>
-                                                                            </div>
-                                                                        ))}
-
-                                                                    </div>
+                                            {occupationCategories.length > 0
+                                                ?
+                                                <div className="row align-items-center">
+                                                    {occupationCategories.map((item) => (
+                                                        <div key={item.id} className="col-lg-4">
+                                                            <div className="card card-background"
+                                                                 style={{backgroundImage: "url(" + item.photo + ")"}}>
+                                                                <div className="card-body text-center">
+                                                                    <Link
+                                                                        to={`/occupations/${occupation.slug}/${item.slug}/`}>
+                                                                        <h2 className="card-title">{item.name}</h2>
+                                                                    </Link>
+                                                                    <Link
+                                                                        to={`/occupations/${occupation.slug}/${item.slug}/`}
+                                                                        className={`btn btn-sm btn-${item.color.name}`}>
+                                                                        En savoir plus
+                                                                    </Link>
                                                                 </div>
                                                             </div>
-                                                        )}
-
-                                                        <div className="row">
-                                                            <div className="col-md-12">
-                                                                <div className={'card-description'}
-                                                                    dangerouslySetInnerHTML={{__html: occupation.description}}/>
-                                                            </div>
+                                                        </div>
+                                                    ))}
+                                                </div>
+                                                :
+                                                <div className="row align-items-center">
+                                                    <div className="col-md-10 mr-auto ml-auto text-center">
+                                                        <div className="kazouTechOccupationEmpty">
+                                                            <i className="ni ni-settings text-red"></i>
+                                                        </div>
+                                                        <div className="mt-5 mb-5">
+                                                            <h2>Les offres pour la catégorie <span className="display-3">{composantTitle}</span> ne sont pas disponible pour le moment.</h2>
                                                         </div>
                                                     </div>
+                                                </div>
+                                            }
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="section features-7 bg-secondary">
+                            <div className="container">
+                                <div className="row justify-content-center">
+                                    <div className="col-lg-12">
+                                        <div className="container">
+                                            <div className="row">
+                                                <div className="col-md-8 mr-auto ml-auto text-center">
+                                                    <h4 className="kazouTech-red-color display-3 mb-5">Nous sommes plus proche de
+                                                        vous !</h4>
+                                                </div>
+                                            </div>
+                                            <CitySite/>
+                                            <br/>
+                                            {allblogbyoccupations.length > 0 && (
+                                                <div className="row">
+                                                    <div className="col-md-12 mx-auto">
+                                                        <h4 className="title">Tous nos articles
+                                                            sur <strong>{occupation.name} </strong> sont sur
+                                                            notre
+                                                            blog</h4>
+                                                        <br/>
+                                                        <div className="row">
+                                                            {allblogbyoccupations.map((item) => (
+                                                                <div className="col-lg-4 col-md-6">
+                                                                    <div className="card card-blog card-plain">
+                                                                        <div className="card-image shadow">
+                                                                            <Link to={`/blog/${occupation.slug}/${item.slug}/`}>
+                                                                                <img className="img rounded"
+                                                                                     src={item.photo}/>
+                                                                            </Link>
+                                                                        </div>
+                                                                        <div className="card-body">
+                                                                            <div className="text-center">
+                                                                                <Link to={`/blog/${occupation.slug}/`} className={`btn btn-sm btn-${item.color.name}`}>
+                                                                                    {occupation.name}
+                                                                                </Link>
+                                                                            </div>
+                                                                            <h6 className="card-title">
+                                                                                <Link to={`/blog/${occupation.slug}/`} >{item.title}</Link>
+                                                                            </h6>
+                                                                            <p className="card-description">
+                                                                                <b dangerouslySetInnerHTML={{__html: (item.body.length > 164 ? item.body.substring(0, 164) + "..." : item.body)}}/>
+                                                                                <Link to={`/blog/${occupation.slug}/${item.slug}/`} >lire la suite</Link>
+                                                                            </p>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            ))}
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            )}
+                                            <div className="row">
+                                                <div className="col-md-12">
+                                                    <div className={'card-description'}
+                                                         dangerouslySetInnerHTML={{__html: occupation.description}}/>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-
-                                    <br/>
                                 </div>
                             </div>
                         </div>
@@ -151,10 +151,8 @@ class OccupationSiteSlug extends Component {
                     <FooterUserSite/>
                 </div>
             </>
-
         )
     }
-
 }
 
 export default OccupationSiteSlug;
