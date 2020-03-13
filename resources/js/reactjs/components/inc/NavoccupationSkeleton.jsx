eac431557dc2c5320b1bb218387a3d349ed22a81
Boclair Temgoua <temgoua2013@gamil.com>
2020-03-13 13:29:56 +0100

    update server

--- resources/js/reactjs/components/inc/NavoccupationSkeleton.jsx
+++ resources/js/reactjs/components/inc/NavoccupationSkeleton.jsx
@@ -4,7 +4,7 @@ import Skeleton from "react-loading-skeleton";
 function NavoccupationSkeleton() {
     return(
         <li className="mb-2">
-            <Skeleton width={250} count={5}/>
+            <Skeleton width={150} count={5}/>
         </li>
     )
 }
